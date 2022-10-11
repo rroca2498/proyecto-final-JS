76ccbbf17a5ebddd8a4bcf4aae9a2d6444268485
rroca2498 <rroca2498@outlook.com>
2022-10-11 14:03:24 -0300

    se agregan las imagenes de los productos en formato svg

--- JS/main.js
+++ JS/main.js
@@ -1,16 +1,3 @@
-// const articulos  = [
-//     {sku: 1, descripcion: "bujia K4M Megane", precio:910 , rubro: "encendido" , img: "../Imagenes/repuestoMuestra.jpg"},
-//     {sku: 2, descripcion: "manija interior Logan", precio: 3800, rubro: "accesorios" , img: "../Imagenes/repuestoMuestra.jpg"},
-//     {sku: 3, descripcion: "kit cremallera embrague R21", precio: 1010, rubro:"accesorios" , img: "../Imagenes/repuestoMuestra.jpg"},
-//     {sku: 4, descripcion: "pata caja R19", precio: 4840, rubro: "motor" , img: "../Imagenes/repuestoMuestra.jpg"},
-//     {sku: 5, descripcion: "ruleman delantero SNR", precio: 7500, rubro: "rodamiento", img: "../Imagenes/repuestoMuestra.jpg"},
-//     {sku: 6, descripcion: "ruleman trasero SKF", precio: 6950, rubro: "rodamiento", img: "../Imagenes/repuestoMuestra.jpg"},
-//     {sku: 7, descripcion: "bieleta Suran", precio: 2870, rubro: "suspension", img: "../Imagenes/repuestoMuestra.jpg"},
-//     {sku: 8, descripcion: "amortiguador delantero Duster", precio: 15640, rubro: "suspension", img: "../Imagenes/repuestoMuestra.jpg"},
-//     {sku: 9, descripcion: "antena universal", precio: 1500, rubro: "accesorios", img: "../Imagenes/repuestoMuestra.jpg"},
-//     {sku: 10, descripcion: "cables de bujia K7M", precio: 11200, rubro: "encendido", img: "../Imagenes/repuestoMuestra.jpg"}
-// ];
-
 const encontrado = document.querySelector("#listaProductos");
 
 const buscador = document.querySelector("#buscador");
@@ -30,7 +17,7 @@ function imprimirProductos(arr){
     div.classList.add('producto');
 
     div.innerHTML = `
-    <img src="../Imagenes/repuestoMuestra.jpg" alt="Imagen de ${producto.descripcion}">
+    <img src="${producto.img}" alt="Imagen de ${producto.descripcion}">
     <h3>${producto.descripcion}</h3>
     <p>$${producto.precio}</p>
     <button id="agregar${producto.sku}">Agregar</button>
